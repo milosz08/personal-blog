f2a96cc1efb69497dffe5a198b61880e94fdbb08
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-08-09 17:52:47 +0200

    feat: replace png hero image into webp format

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import heroImage from '@/assets/hero.png';
+import heroImage from '@/assets/hero.webp';
 
 const Hero: React.FC = (): React.ReactElement => (
   <Image src={heroImage} alt="hero" className="rounded-2xl" />
